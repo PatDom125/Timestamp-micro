4ad7b3f4e276551c265e036cac7a0bb80306a572
pat <patdom125@gmail.com>
2017-03-12 06:39:34 +0100

    Added moment, working on converter

--- converter.js
+++ converter.js
@@ -1,4 +1,14 @@
+'use strict';
+var moment = require('moment');
+
 module.exports = function converter(queryStr){
   // Takes string, if it is unix timestamp it creates natural representation
   // and vice versa. Returns object with both formats.
-}
+  var result = {};
+
+  if(Number(queryStr)){
+    result.unix = Number(queryStr);
+    result.natural = moment(Number(queryStr)).unix();
+  } 
+
+};
