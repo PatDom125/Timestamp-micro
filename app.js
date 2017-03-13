61cefaebfbe4b2ba7da9cc02c25c035400e8ef04
pat <patdom125@gmail.com>
2017-03-13 08:19:12 +0100

    heroku

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var converter = require('./converter');
 
 var port = process.env.PORT || 3000;
 
-app.use('/', express.static(__dirname + '/public'));
-
 app.get('/', function(req, res){
 
   res.sendFile(__dirname + '/index.html');
